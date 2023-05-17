82fd6b518596f5907e2bb501a1e85c53ec93e7fc
NadaHab <91700494+NadaHab@users.noreply.github.com>
2023-05-17 09:07:29 -0400

    fixing responsive effect on mobile: add top paddin

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -71,14 +71,16 @@ const Experience = () => {
   return (
     <div
       name="experience"
-      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
+      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen pt-40"
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
           <p className="text-4xl font-bold border-b-4  text-blue-500 border-gray-500 p-2 inline">
             Experience
           </p>
-          <p className="text-xl py-6">These are the technologies I've worked with:</p>
+          <p className="text-xl py-6">
+            These are the technologies I've worked with:
+          </p>
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
