bce41c36581c588c04a66ebb96bd26acc3b00cff
NadaHab <91700494+NadaHab@users.noreply.github.com>
2024-01-29 10:13:34 -0500

    About Section modified table

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,22 @@
 import React from "react";
+import { useState } from "react";
+import "./AboutCss/style.css";
+
+const content = [
+  [
+    "I am a Full Stack Developer",
+    "Passionate for front-end and back-end development",
+    "Amazed by the continuous development in the world of Software",
+    "I am a lover of studying the new technologies!",
+  ],
+  ["React", "Java", "Front-end development", "SQL and MySQL"],
+  ["Microsoft PowerPoint", "Microsoft Excel", "Microsoft Word", " "],
+  ["Home Decoration", "Planting", "Jogging", "Listening to music "],
+];
 
 const About = () => {
+  const [activeContentIndex, setActiveContentIndex] = useState(0);
+
   return (
     <div
       name="about"
@@ -13,23 +29,53 @@ const About = () => {
           </p>
         </div>
 
-        <p className="text-white text-xl mt-20 ">
-          I am a Full Stack Developer, passionate for front-end and back-end
-          development. Amazed by this world of continuous development, I am a
-          lover of studying the new technologies!
-        </p>
-
         <br />
 
         <p className="text-white text-xl ">
-          With experience in Java, C# (.Net and .Net Core), PHP, JavaScript,
-          HTML, CSS, SQL. Visual Studio, VSCode, IntelliJ IDEA. Frameworks:
-          Asp.net(MVC and Razor Pages), WPF, MySQL, React. I can Handle SQL
-          databases such as MySQL and MS SQL Server and, no-SQL databases:
-          MongoDB. I can Create and consume web API, webservices, knowledge of
-          AWS, Unit testing, Git and Github, Agile methodology, Trello.
-          Proficient in Microsoft Office applications (Powerpoint, Excel, Word).
+          I am a Full Stack Developer / Software Developer.       <br />
+          With experience in Java, C# (.Net and .Net Core), PHP, JavaScript, HTML, CSS, SQL. Visual
+          Studio, VSCode, IntelliJ IDEA. Frameworks: Asp.net(MVC and Razor
+          Pages), WPF, MySQL, React. I can Handle SQL databases such as MySQL
+          and MS SQL Server and, no-SQL databases: MongoDB. I can Create and
+          consume web API, webservices, knowledge of AWS, Unit testing, Git and
+          Github, Agile methodology, Trello. Proficient in Microsoft Office
+          applications (Powerpoint, Excel, Word).
         </p>
+        <div id="tabs">
+          <menu>
+            <button
+              className={activeContentIndex === 0 ? "active" : ""}
+              onClick={() => setActiveContentIndex(0)}
+            >
+              Who Am I?
+            </button>
+            <button
+              className={activeContentIndex === 1 ? "active" : ""}
+              onClick={() => setActiveContentIndex(1)}
+            >
+              My Passion
+            </button>
+            <button
+              className={activeContentIndex === 2 ? "active" : ""}
+              onClick={() => setActiveContentIndex(2)}
+            >
+              Microsoft
+            </button>
+            <button
+              className={activeContentIndex === 3 ? "active" : ""}
+              onClick={() => setActiveContentIndex(3)}
+            >
+              Hobbies
+            </button>
+          </menu>
+          <div id="tab-content">
+            <ul>
+              {content[activeContentIndex].map((item) => (
+                <li key={item}>{item}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
